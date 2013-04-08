0624bb34d97670b4659023f7c028a7eb1cb12b00
Carlos Rosario <gzcarlosrd@gmail.com>
2013-04-08 11:28:37 -0400

    Defined the Filer class
    Defined the Filer class with it's attributes and methods.
    attributes = file (java.io.File)
    methods = Filer(String archivo); [Constructor], readFile() (String[])

--- src/needs/Filer.java
+++ src/needs/Filer.java
@@ -1,5 +1,42 @@
 package needs;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Filer {
+	private File file;
+	private String takenFile = "";
+	
+	public Filer(String archivo){
+		file = new File(archivo);
+	}
+	
+	public String[] readFile(){
+		String[] lines = new String[42];
+		int linesPointer = 0;
+		
+		try{
+			
+			FileReader freader = new FileReader(file);
+			BufferedReader reader = new BufferedReader(freader);
+			String line = "";
+			
+			while((line = reader.readLine()) != null){
+				lines[linesPointer] = line;
+				++linesPointer;
+			}
+			
+			
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		
+		return lines;
+	}
 
 }
