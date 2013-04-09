123ae1f752d771b0d9cccf30bdc6f8ebe269bfc6
Carlos Rosario <gzcarlosrd@gmail.com>
2013-04-09 16:09:46 -0400

    UnCommented - in GamePlayedTest
    Cahnged a test signature from 'testPlayers' to 'testPlayer1' according
    to the task that's performed in that method.
    
    Also unCommented the 'for' loop of the method mentioned before.

--- src/forTest/GamePlayedTest.java
+++ src/forTest/GamePlayedTest.java
@@ -17,19 +17,19 @@ public class GamePlayedTest {
 	
 
 	@Test
-	public void testPlayers(){
+	public void testPlayer1(){
 		Filer filer = new Filer("C:/Users/Carlos/Documents/Workspace/BowlingTextFile/file1.txt");
 		GamePlayed gp = new GamePlayed(filer);
 		int[] p1 = {10,0,10,0,7,2,9,1,10,0,8,0,5,5,9,1,10,0,9,1,7};
 		int[] capturedP1 = gp.getPlayer1();
 		boolean same = true;
 		
-		//for(int i = 0; i < 21; ++i)
-			//if(capturedP1[i] != p1[i]){
-				//System.out.println("Captured[" + i + "]: " +  capturedP1[i] + ", Pointed[" + i + "]: " + p1[i]);
-				//same = false;
-				//break;
-			//}
+		for(int i = 0; i < 21; ++i)
+			if(capturedP1[i] != p1[i]){
+				System.out.println("Captured[" + i + "]: " +  capturedP1[i] + ", Pointed[" + i + "]: " + p1[i]);
+				same = false;
+				break;
+			}
 		
 		assertEquals(capturedP1, p1);
 		assertTrue(same);
