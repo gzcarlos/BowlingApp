bd0b6d6e095ec3bb9bad8a2ec3d661c20d0badca
Teodoro <teodoro641@hotmail.com>
2013-04-13 22:30:50 -0400

    Implementation of Game Rules (in Ruler class), added the LogicFrame class and a few modifications to the GUI and Frame classes

--- src/needs/Frame.java
+++ src/needs/Frame.java
@@ -1,5 +1,61 @@
-package needs;
+package bowling;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 
-public class Frame {
+import javax.swing.JLabel;
+import javax.swing.JLabel;
 
+@SuppressWarnings("serial")
+public class Frame extends javax.swing.JPanel {
+	
+
+	private JLabel FS;				// First Shoot Label
+	private JLabel SS;				//Second Shoot Label
+	private JLabel TS;				// Third Shoot Label
+	private JLabel TP;				// Total points Label
+	boolean addsapce = true;
+	private GridBagConstraints c = new GridBagConstraints();		//Initializes  the gridbagcosntraints to locate the labels in the panel
+	
+	public Frame (boolean add){
+		setPreferredSize(new Dimension(100,100));
+		
+		setLayout(new GridBagLayout());
+		FS = new JLabel("X");
+		c.gridx = 1;
+		c.gridy = 1;
+		add(FS,c);
+		
+		SS = new JLabel("X");
+		c.gridx = 5;
+		c.gridy = 1;
+		add(SS,c);
+		
+		TS = new JLabel("X");
+		c.gridx = 5;
+		c.gridy = 3;
+		add(TS,c);
+	}
+	
+	public Frame (){
+		setPreferredSize(new Dimension(85,85));
+		
+		setLayout(new GridBagLayout());
+		FS = new JLabel("X");
+		c.gridx = 1;
+		c.gridy = 1;
+		add(FS,c);
+		SS = new JLabel("X");
+		c.gridx = 3;
+		c.gridy = 1;
+		add(SS,c);
+		TP = new JLabel("X");
+		c.gridx = 2;
+		c.gridy = 3;
+		add(TP,c);
+		
+		
+	}
 }
--- src/needs/GUI.java
+++ src/needs/GUI.java
@@ -1,5 +1,121 @@
-package needs;
+package bowling;
+import javax.swing.*;
 
-public class GUI {
+import java.awt.*;
+@SuppressWarnings("serial")
+public class GUI extends JFrame{
 
+	/**
+	 * @param args
+	 * @author Teodoro
+	 */
+	
+	private JPanel organizemiddle (){
+		
+		JLabel P1Tag,P2Tag;
+		Frame Points[] = new Frame[10];
+		JPanel Player1 = new JPanel();
+		JPanel Player2 = new JPanel();
+		JPanel organize = new JPanel();
+		
+		GridBagConstraints j = new GridBagConstraints();
+		Player1.setLayout(new GridLayout(0,10));
+		for (int i=0; i<10;++i){
+			Points[i] = new Frame();
+			
+			Player1.add(Points[i],j);
+		}
+		
+		Player2.setLayout(new GridLayout(0,10));
+		for (int i=0; i<10;++i){
+			Points[i] = new Frame();
+			Player2.add(Points[i]);
+		}
+		
+		
+		organize.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		P1Tag = new JLabel("Jugador 1: ");
+		c.anchor = GridBagConstraints.WEST;
+		c.gridx = 0;
+		c.gridy = 0;
+		organize.add(P1Tag,c);
+		
+		c.anchor = GridBagConstraints.CENTER;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.insets = new Insets(10, 0, 10, 0);
+		organize.add(Player1,c);
+		
+		P2Tag = new JLabel("Jugador 2: ");
+		c.anchor = GridBagConstraints.WEST;
+		c.gridx = 0;
+		c.gridy = 2;
+		organize.add(P2Tag,c);
+		
+		c.anchor = GridBagConstraints.CENTER;
+		c.gridx = 0;
+		c.gridy = 3;
+		c.gridwidth = 9;
+		organize.add(Player2,c);
+		
+		return organize;
+	}
+	
+	@SuppressWarnings("unused")
+	public void UI(){
+		
+		GridBagLayout bag = new GridBagLayout() ;
+		Container cont = new Container();
+		cont.setSize(5, 5);
+		
+		JPanel Background = new JPanel(bag);
+		//Background.setBounds(this.getX(), this.getY(), this.getWidth()-10, this.getHeight());
+		this.getContentPane().add(Background);
+		//setSize(400, 300);
+		JLabel sentence;
+		JButton explore,prior,next;
+		
+		JPanel top = new JPanel();
+		top.add(sentence = new JLabel("Seleccione un Archivo"));
+		top.add(explore = new JButton("Explorar"));
+		
+		JPanel middle = new JPanel();
+		//middle.setPreferredSize(new Dimension(1500,720));
+		middle.add(organizemiddle());
+		
+		JPanel bottom = new JPanel();
+		bottom.add(prior = new JButton("Atras"));
+		bottom.add(next = new JButton("Siguiente"));
+		
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.anchor = GridBagConstraints.WEST;
+		c.gridx = 0;
+		c.gridy = 0;
+		Background.add(top,c);
+		
+		
+		c.anchor = GridBagConstraints.CENTER;
+		c.gridx = 0;
+		c.gridy = 2;
+		Background.add(middle,c);
+		
+		c.gridx = 0;
+		c.gridy = 3;
+		Background.add(bottom,c);
+		
+		this.pack();
+		this.setResizable(false);
+		this.setVisible(true);
+		
+	}
+	
+	public GUI(){
+		UI();
+	}
+	
 }
+
+
